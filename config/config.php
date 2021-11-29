40d12e616c45bba334ec2bc1cbec8e8ec566dd60
lucas-barbosa-ifsp <93929780+lucas-barbosa-ifsp@users.noreply.github.com>
2021-11-29 15:05:33 -0300

    V0.0.1

--- config/config.php
+++ config/config.php
@@ -7,6 +7,6 @@ class config
         return "https://200.133.222.32:8080";
     }
     public static function frontUri(){
-        return "http://200.133.222.32/inventory-front-php";
+        return "http://200.133.222.32:/inventory-front-php";
     }
 }
