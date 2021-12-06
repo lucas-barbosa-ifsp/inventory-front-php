6fdc993fdb24273cff333d5484229ec6835300d2
lucas-barbosa-ifsp <93929780+lucas-barbosa-ifsp@users.noreply.github.com>
2021-12-06 11:50:26 -0300

    V0.0.1

--- template/footer.php
+++ template/footer.php
@@ -12,14 +12,14 @@
 
 
         $(".tile-bloco").click(function (){
-            window.location.replace('http://10.115.50.63:8080/classes.php?bloco='+ $(this).attr("data-id"))
+            window.location.replace('http://200.133.222.32/inventory-front-php/classes.php?bloco='+ $(this).attr("data-id"))
         })
         $(".tile-class").click(function (){
-            window.location.replace('http://10.115.50.63:8080/workspace.php?class='+ $(this).attr("data-id") + "&name=" + $(this).attr("data-name") )
+            window.location.replace('http://200.133.222.32/inventory-front-php/workspace.php?class='+ $(this).attr("data-id") + "&name=" + $(this).attr("data-name") )
         })
 
         $(".search").click(function (){
-            window.location.replace('http://10.115.50.63:8080/workspace.php?' +
+            window.location.replace('http://200.133.222.32/inventory-front-php/workspace.php?' +
                 'class='+ $(this).attr("data-class") +
                 "&name=" + $(this).attr("data-name") +
                 "&pesquisa=" + $(".pesquisa-value").val()
