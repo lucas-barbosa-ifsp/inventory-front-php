2a9281f9edb389c014d09010f914e0fceb2aae1b
lucas-barbosa-ifsp <93929780+lucas-barbosa-ifsp@users.noreply.github.com>
2021-12-15 16:46:23 -0300

    V 0.0.1-1

--- index.php
+++ index.php
@@ -28,16 +28,16 @@ if(isset($_COOKIE['token'])){
             <?php } ?>
             <div class="col-10 offset-1 mt-5 md-5 mr-5 ml-5 border border-success rounded">
                 <h3 class="text-center mb-5 mt-5">Entrar no sistema de inventário - IFSP-SÃO CARLOS</h3>
-                <div class="col-6 offset-3 mb-5">
+                <div class="col-10 offset-1 mb-5">
                     <form action="/register.php">
                         <div class="mb-3 ">
                             <label for="formGroupExampleInput" class="form-label">Digite seu email</label>
-                            <input type="text" name = "email" class="form-control" id="formGroupExampleInput"
+                            <input type="email" name = "email" class="form-control" id="formGroupExampleInput"
                                    placeholder="Email">
                         </div>
                         <div class="mb-3">
                             <label for="formGroupExampleInput2"  class="form-label">Digite seu código</label>
-                            <input type="text" class="form-control" name = "accessCode" id="formGroupExampleInput2"
+                            <input type="password" class="form-control" name = "accessCode" id="formGroupExampleInput2"
                                    placeholder="Código de Acesso">
                         </div>
                         <button type = "submit" class="btn btn-success">Entrar</button>
