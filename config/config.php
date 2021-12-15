b66b1030a50012a4189dd8bd6e01d324b600cdc0
lucas-barbosa-ifsp <93929780+lucas-barbosa-ifsp@users.noreply.github.com>
2021-12-15 16:47:18 -0300

    V 0.0.1-1

--- config/config.php
+++ config/config.php
@@ -3,16 +3,16 @@
 class config
 {
 
-//    public static function uri(){
-//        return "http://10.115.50.63:8080";
-//    }
-//    public static function frontUri(){
-//        return "http://200.133.222.32/";
-//    }
     public static function uri(){
-        return "http://localhost:8080/";
+        return "http://10.115.50.63:8080";
     }
     public static function frontUri(){
-        return "http://localhost:3000/";
+        return "http://200.133.222.32/";
     }
+//    public static function uri(){
+//        return "http://localhost:8080/";
+//    }
+//    public static function frontUri(){
+//        return "http://localhost:3000/";
+//    }
 }
